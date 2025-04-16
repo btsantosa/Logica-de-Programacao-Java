202a7ae5b770c47aa63aec183fe935ee19fe9cdf
Brena Santos <frescotomate123@gmail.com>
2025-04-15 23:07:07 -0300

    Atualização de atividade e criação da pasta para exercicios de de metodo

---
+++ 2.Def/Atividade1.java
@@ -0,0 +1,17 @@
+import  java.util.Random;
+import java.util.Scanner;
+public class Atividade1{
+    public static void main(String[]args){
+
+        // Crie um função para gerar numeros aleatorio e o usuario ira escolher o numero inicial e final do sorteio
+        System.out.printf("O numero aleatório é %d", numero);
+    }
+    public static int geradorNumero(int min, int max){
+        Random aleatorio = new Random();
+        Scanner entrada = new Scanner(System.in);
+
+        int min = entrada.nextInt();
+        int max = entrada.nextInt();
+    
+    }
+}
